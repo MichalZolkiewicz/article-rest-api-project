619518f86c8408ec32992b4476964e28a0699c35
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2022-09-08 23:48:42 +0200

    RestController with dummy methods

---
+++ src/main/java/com/example/articlerestapi/controller/ArticleController.java
@@ -0,0 +1,46 @@
+package com.example.articlerestapi.controller;
+
+import com.example.articlerestapi.domain.ArticleDto;
+import com.example.articlerestapi.service.ArticleServiceManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/article")
+@RequiredArgsConstructor
+public class ArticleController {
+
+    private final ArticleServiceManager articleServiceManager;
+
+    @GetMapping(value = "getArticles")
+    public List<ArticleDto> getAllArticles() {
+        return articleServiceManager.getAllArticles();
+    }
+
+    @GetMapping(value = "getCopy")
+    public ArticleDto getArticle(@RequestParam Long articleId) {
+        return articleServiceManager.getArticleDto(articleId);
+    }
+
+    @PostMapping(value = "addArticle")
+    public Long addArticle(@RequestBody ArticleDto articleDto) {
+        return articleServiceManager.addArticle(articleDto);
+    }
+
+    @PutMapping(value = "updateArticle")
+    public void updateArticle(@RequestParam Long articleId, @RequestBody ArticleDto articleDto){
+        articleServiceManager.updateArticle(articleId, articleDto);
+    }
+
+    @DeleteMapping(value = "deleteArticle")
+    public void deleteArticle(@RequestParam Long articleId) {
+        articleServiceManager.deleteArticle(articleId);
+    }
+
+    @GetMapping(value = "searchArticleKeyword")
+    public List<ArticleDto> searchArticleKeyword(@RequestParam String keyword) {
+        return articleServiceManager.searchArticleKeyword(keyword);
+    }
+}
