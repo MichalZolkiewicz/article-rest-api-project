42b174d139a65424548d67b65b150450984b580a
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2022-09-08 23:48:21 +0200

    Repository interface with Crud extend. findAll() and save() methods overridden.

---
+++ src/main/java/com/example/articlerestapi/repositories/ArticleRepository.java
@@ -0,0 +1,24 @@
+package com.example.articlerestapi.repositories;
+
+import com.example.articlerestapi.domain.Article;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Repository
+public interface ArticleRepository extends CrudRepository <Article, Integer> {
+
+    Article findById(Long id);
+
+    @Override
+    List<Article> findAll();
+
+    @Override
+    Article save(Article article);
+
+    void deleteById(Long id);
+
+}
