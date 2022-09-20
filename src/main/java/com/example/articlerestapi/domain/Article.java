600747648c0908255bbc60f7fe1ce010db58bc0b
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2022-09-20 22:58:39 +0200

    @Column annotation added

--- src/main/java/com/example/articlerestapi/domain/Article.java
+++ src/main/java/com/example/articlerestapi/domain/Article.java
@@ -3,6 +3,7 @@ package com.example.articlerestapi.domain;
 import com.sun.istack.NotNull;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -19,12 +20,25 @@ public class Article {
     @Id
     @NotNull
     @GeneratedValue
+    @Column(name = "ID", unique = true)
     private Long id;
+
+    @Column(name = "ARTICLE_TITLE")
     private String articleTitle;
+
+    @Column(name = "ARTICLE_CONTENT")
     private String articleContent;
+
+    @Column(name = "DATE_OF_PUBLICATION")
     private LocalDate dateOfPublication;
+
+    @Column(name = "MAGAZINE_NAME")
     private String magazineName;
+
+    @Column(name = "AUTHOR")
     private String authorFirstAndLastName;
+
+    @Column(name = "TIMESTAMP")
     private Timestamp timestamp;
 
     public Article(Long id, LocalDate dateOfPublication, String magazineName, Timestamp timestamp) {
